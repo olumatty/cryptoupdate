0888881809848cdc266165b703917b11aab2daee
Matthew Adebayo <olumatty@gmail.com>
2024-11-28 19:45:36 +0100

    fourth commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import CryptoSearch from '@/components/CryptoSearch'
+import CryptoUpdate from '@/components/CryptoUpdate'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <CryptoSearch/>
+      <CryptoUpdate/>
     </div>
   )
 }
--- src/components/CryptoUpdate.jsx
+++ src/components/CryptoUpdate.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
-const CryptoUpdate = () => {
+
+const CryptoUpdate = ({name,symbol,price, volume, priceChange, marketCap}) => {
   return (
-    <div className='flex justify-center'>
-        <div className='flex flex-row border-b justify-start items-center w-[900px] h-[80px]'>
-            <div className='flex items-center min-w-[300px] pr-8 '>
+    <div className="flex justify-start">
+      <div className="flex flex-row border border-[#f97316] border-b items-center w-auto md:w-auto m-3">
 
-            </div>
-
+        <div className="overflow-x-auto w-full">
+          <table className="table-auto min-w-full border-collapse">
+            <thead>
+              <tr className="border-b text-sm font-semibold">
+                <th className="pl-3 pr-10">Crypto</th>
+                <th className="pr-10">Name</th>
+                <th className="pr-10">Symbol</th>
+                <th className="pr-10">Price</th>
+                <th className="pr-10">Volume</th>
+                <th className="pr-10">Price Change</th>
+                <th className="pr-10">Market Cap</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="text-center">
+                <td className="pl-5 pr-10">
+                  <Image src={image} alt='crypto'/>
+                </td>
+                <td className="pr-10">{name}</td>
+                <td className="pr-10">{symbol}</td>
+                <td className="pr-10">${price}</td>
+                <td className="pr-10">${volume}</td>
+                {priceChange<0 ? (<td className='text-red-500'>{priceChange.toFixed(2)}%</td>): (<td className='text-green-500'>{priceChange.toFixed(2)}%</td>)}
+                <td className="pr-10">${marketCap.tolocaleString()}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CryptoUpdate
+export default CryptoUpdate;
