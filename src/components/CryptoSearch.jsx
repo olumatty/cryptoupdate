e50e82a8306e0580395354cc87d04820b88b4202
Matthew Adebayo <olumatty@gmail.com>
2024-11-28 16:13:57 +0100

    Third commit

--- src/components/CryptoSearch.jsx
+++ src/components/CryptoSearch.jsx
@@ -8,8 +8,8 @@ const CryptoSearch = () => {
       <div className='mb-12 flex flex-col flex-wrap items-center justify-center '>
         <h1 className='text-4xl md:text-6xl font-bold text-orange-500'>CRYPTO UPDATE</h1>
         <form>
-            <input type='text' value={cryptoName} onChange={(e) => {setCryptoName(e.target.value)}} placeholder='Example: Bitcoin Cash' 
-            className=' w-[320px] mt-3 h-12 p-4 font-medium md:w-[600px]'/>
+            <input type='text' value={cryptoName} onChange={(e) => {setCryptoName(e.target.value)}} placeholder='Example: Bitcoin' 
+            className=' w-[320px] mt-3 h-14 p-4 font-medium md:w-[600px] text-neutral-500 rounded-full outline-none border border-[#f97316]'/>
         </form>
       </div>
     </div>
---
+++ src/components/CryptoUpdate.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CryptoUpdate = () => {
+  return (
+    <div className='flex justify-center'>
+        <div className='flex flex-row border-b justify-start items-center w-[900px] h-[80px]'>
+            <div className='flex items-center min-w-[300px] pr-8 '>
+
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default CryptoUpdate
